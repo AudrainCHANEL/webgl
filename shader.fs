27eafc0d3096987868711e407832ce87cc4f34c6
AudrainCHANEL <audrain.chanel21000@gmail.com>
2021-09-07 10:56:44 +0200

    ajout fausse couleur

--- shader.fs
+++ shader.fs
@@ -7,15 +7,32 @@ uniform sampler2D uSampler;
 uniform float uSeuilMin;
 uniform float uSeuilMax;
 
-void main(void) {
+const float lut = 0.9;
 
+void main(void) {
+	float alpha;
 	vec4 texture = texture2D(uSampler, vec2(tCoords.s, tCoords.t));
 
 	if(uSeuilMin < texture[0] && uSeuilMax > texture[0]) {
-		gl_FragColor = vec4(vec3(texture), texture[0]);
+		alpha = texture[0];
 	}
 	else{
-		gl_FragColor = vec4(vec3(texture), 0.0);
+		alpha = 0.0;
 	}	
 	
+
+	if (lut != 0.0) {
+		if (texture[0] < lut) {
+			texture[0]=texture[0]/lut;
+			texture[1]=0.0;
+			texture[2]=0.0;
+		}
+		else {
+			texture[0] = (1.0-texture[0])/lut;
+			texture[1] = 0.0;
+			texture[2] = 0.0;
+		}
+	}
+
+	gl_FragColor = vec4(vec3(texture), alpha);
 }
